32ed4ac75a0f62605a286260c520193127c3cb9a
kiyoungchoi <kaist6123@naver.com>
2018-08-05 00:45:33 +0900

    fro

--- guide01.js
+++ guide01.js
@@ -27,7 +27,22 @@ $('#someDiv').on('ifToggled', function(event){
 $('#someDiv').val().substr(0,7)
 // 처럼 활용 할수 있다. substr(시작, 길이)을 잘 활용하자.
 
+
 // 재직중일땐, icheck('check')로 만들고, 
 var end_value = 'some date';
 $('#theEndDateDiv').hide();
 // 처럼 가짜값으로 넣어서 값인식만 하도록 넣어 두었다. 
+
+
+// 입력된 숫자와 관련한 내용일땐
+// 특별히 숫자의 한계를 걸어둘때,
+a= dat.replace(/-/g,'');
+b= parseInt(a)
+date_standard = '0000000'
+
+if(dat!){
+
+}else if(dat < date_standard){
+	alert('again plz');
+	$('#someInput').focus();
+}
